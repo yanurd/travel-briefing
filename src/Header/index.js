e2916a1d5bb53b1fefd0d2931fc79f298c1cd94f
Yancarlo Urdaneta <yancarlou@gmail.com>
2021-09-15 09:39:41 -0400

    Adding contact me hyperlink

--- src/Header/index.js
+++ src/Header/index.js
@@ -11,8 +11,7 @@ const Header = () => {
           Travel<span className="header-title_secondary">briefing</span>
         </h1>
         <div className="header-contact">
-          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a className="header-contact_me" href="#">Contact me</a>
+          <a className="header-contact_me" href="https://twitter.com/yanurd_">Contact me</a>
         </div>
       </section>
     </header>
